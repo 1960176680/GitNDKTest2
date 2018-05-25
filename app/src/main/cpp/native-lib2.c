aead9240f766e10c74cb18eeebe1664ebe26624c
Sun <1960176680@qq.com>
2018-05-25 18:00:38 +0800

    提交
    提交

--- app/src/main/cpp/native-lib2.c
+++ app/src/main/cpp/native-lib2.c
@@ -11,9 +11,13 @@ jstring Java_com_zhouwenguang_hz_gitndktest_MainActivity_sayHello(JNIEnv* env,jo
 
 jintArray Java_com_zhouwenguang_hz_gitndktest_MainActivity_add(JNIEnv* env,jobject obj){
     char *as="I like jni";
-    int array[]={6,2,4,1,3,5};
+    int a=3;
+    int b=6;
+    int c;
+    c=a+b;
+
 //    jsize       (*GetArrayLength)(JNIEnv*, jarray);
 //    jintArray     (*NewIntArray)(JNIEnv*, jsize);
-    jsize a=(*env)->GetArrayLength(env,array);
+
     return (*env)->NewIntArray(env,a);
 }
