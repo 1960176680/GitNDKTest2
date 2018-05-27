cf89639a115e497a7c13696d10a480035528107b
太阳Sun <1960176680@qq.com>
2018-05-27 22:37:44 +0800

    提交
    提交

---
+++ soimport/src/main/java/com/example/soimport/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.soimport;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    static {
+        System.loadLibrary("native-lib2");
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView textView=findViewById(R.id.tv);
+        textView.setText(sayHello());
+    }
+    public native String sayHello();
+}
